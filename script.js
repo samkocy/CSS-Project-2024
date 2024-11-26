fa7caed2750c8f83081c64599cbbe88f5e6e5f94
samkocy <s00255627@atu.ie>
2024-11-26 20:00:46 +0000

    creating funciton to fetch data from the api

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ const apiKey = '3OTra5WFEIv6rl-RnxnwwTIuDPJoLWuWuU81eFZSKc0';
 const apiHost = 'https://api.unsplash.com';
 
 
+// function to fetch data from the API
+async function fetchRandomPhotos() {
+
+    // url for the api request
+    const url = '';
+
+
+    try {
+        // sends GET request
+        const response = await fetch(url);
+
+        // converts JSON file to JS object
+        const data = await response.json();
+    }
+    catch (error) {
+        // if an error occurs, its caught here
+        console.error(error);
+    }
+}
